ae3a55f46af5b39a69f78ea12d6df69c8f41d2e4
ThiwankiDias <thiwanki.2019741@iit.ac.lk>
2023-04-25 10:52:31 +0530

    new update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # developer: H. Thiwanki Dias Hettiarachchi (UoW id : w1790191)
+# used python version for the project = 3.7.2
+
 import os
-import flask
 from flask import Flask, request, render_template, redirect, send_file, url_for, jsonify
 import io
 from PIL import Image
 import torch
-import argparse
 import yolov5
 
 app = Flask(__name__, template_folder='D:/FYP/w1790191_full_code/w1790191_backend_api/template',
@@ -17,6 +17,7 @@ app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
 @app.route("/predict_defects", methods=['GET', 'POST'])
 def predict_defects():
+    # check whether the request.method is POST
     if request.method == 'POST':
         if "file" not in request.files:
             return redirect(request.url)
@@ -30,12 +31,14 @@ def predict_defects():
         img_save_name = f"D:/FYP/w1790191_full_code/w1790191_backend_api/static/images/pred_defects_image.jpg"
         Image.fromarray(results.ims[0]).save(img_save_name)  # save the predicted image.
 
-        return send_file(img_save_name, mimetype='image/jpg')
+        return send_file(img_save_name, mimetype='image/jpg')  # return the response (predicted saved image) to the user
+    # if the request.method is GET
     return render_template("index1.html")
 
 
 @app.route("/predict_risks", methods=['GET', 'POST'])
 def predict_risks():
+    # check whether the request.method is POST
     if request.method == 'POST':
         if "file" not in request.files:
             return redirect(request.url)
@@ -48,13 +51,15 @@ def predict_risks():
         results.render()[0]
         img_save_name = f"D:/FYP/w1790191_full_code/w1790191_backend_api/static/images/pred_defects_risks_image.jpg"
         Image.fromarray(results.ims[0]).save(img_save_name)  # save the predicted image.
-        return send_file(img_save_name, mimetype='image/jpg')
+        return send_file(img_save_name, mimetype='image/jpg') # return the response (predicted saved image) to the user
+    # if the request.method is GET
     return render_template("index2.html")
 
 
 @app.route("/predict_overall_risk", methods=['GET', 'POST'])
 def predict_overall_risk():
-    if flask.request.method == 'POST':
+    # check whether the request.method is POST
+    if request.method == 'POST':
         if request.files.get("file"):
             img_file = request.files["file"]
             img_bytes = img_file.read()
@@ -62,15 +67,21 @@ def predict_overall_risk():
             results = model_of_defects_risk_detection([img], size=640)
             prediction = results.pandas().xyxy[0]
             if len(prediction) > 0:
+                # extract the list of detected class names from the prediction
                 class_names = prediction["name"].tolist()
+                # defined the order
                 risk_order = ["high_risk", "medium_risk", "low_risk"]
+                # sort the class names according to the above defined order
                 class_names.sort(key=risk_order.index)
-                highest_risk = class_names[0]
+                highest_risk = class_names[0] # risk level of the defective tyre
             else:
+                # if there are not any detected risk levels in image (if the tyre is not defective) the risk level of
+                # the defective tyre assigned to "no risk"
                 highest_risk = "no risk"
                 # results_dict = {"prediction": results.pandas().xyxy[0].to_dict(orient="records")}
                 # return flask.jsonify(results_dict)
             return jsonify({"risk_level": highest_risk})
+    # if the request.method is GET
     return render_template("index3.html")
 
 
